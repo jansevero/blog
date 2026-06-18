a1d23c5ec55be2281aa26fb3b82b4c009c46cf64
jansevero <jansevero@gmail.com>
2026-06-17 21:08:59 -0300

    Aula 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
 export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1>
-      <p className="mt-4 text-lg text-gray-600">This is the home page.</p>
-    </main>
-  );
+  return <h1>Olá de dentro de page.tsx</h1>;
 }
